18ec142205dd4885c27ace534d3f7e46c28508ea
kamwaro <petersonkamwaro@gmail.com>
2020-05-14 02:06:26 +0300

    really understoodhow to use the fetch api as well as using async await

--- promise4.js
+++ promise4.js
@@ -2,16 +2,14 @@ async function getUsers() {
   const res = await fetch('https://api.github.com/users');
   const data = await res.json();
 
-  let all = '';
+  let users = '';
+
+  data.forEach((user) => {
+    users += `<li>Id: ${user.id} User: <img src="${user.avatar_url}" width="100px" height="100px"></li>`;
+  });
 
   console.log(data);
-
-  data.forEach(
-    (users) =>
-      (all += `<li>Name: ${users.login} <img src=${users.avatar_url}> </img></li>`)
-  );
-
-  document.body.innerHTML = all;
+  document.body.innerHTML = users;
 }
 
 getUsers();
