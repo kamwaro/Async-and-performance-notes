a14573691b81c80a68f24e8d6437d32a2e2f4c76
kamwaro <petersonkamwaro@gmail.com>
2020-05-14 16:44:19 +0300

    added css styles

--- promise4.js
+++ promise4.js
@@ -2,13 +2,15 @@ async function getUsers() {
   const res = await fetch('https://api.github.com/users');
   const data = await res.json();
 
+  console.log(data);
+
   let users = '';
 
-  data.forEach((user) => {
-    users += `<li>Id: ${user.id} User: <img src="${user.avatar_url}" width="100px" height="100px"></li>`;
-  });
+  data.forEach(
+    (user) =>
+      (users += `<div class="user">Name:${user.login}<img src=${user.avatar_url} width="100px" height="100px"></img></div>`)
+  );
 
-  console.log(data);
   document.body.innerHTML = users;
 }
 
